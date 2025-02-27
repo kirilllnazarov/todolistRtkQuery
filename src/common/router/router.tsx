a9c342929e3f4a53bd6f67fd266df88b643f9937
Kirill <kirill0392@gmail.com>
2025-02-27 18:24:00 +0300

    add createHashRouter, deploy

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -1,5 +1,5 @@
 import { Page404 } from "common/components"
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import { App } from "../../app/App"
 import { Main } from "../../app/Main"
 import { Login } from "../../features/auth/ui/Login/Login"
@@ -8,7 +8,7 @@ export const Path = {
   Login: "login",
 } as const
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <App />,
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { router } from "common/router"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import { Provider } from "react-redux"
-import { RouterProvider } from "react-router-dom"
+import { HashRouter, RouterProvider } from "react-router-dom"
 import { store } from "./app/store"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
@@ -11,7 +11,3 @@ root.render(
     <RouterProvider router={router} />
   </Provider>,
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
